aa582dbe4a361dfb30b517631cc9567aedd2889f
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-05-01 13:20:44 -0400

    Reformatting and cleanup of gnat pretty settings.

--- src/sp-contexts.adb
+++ src/sp-contexts.adb
@@ -3,33 +3,30 @@ with Ada.Strings.Unbounded.Text_IO;
 with Ada.Text_IO;
 
 package body SP.Contexts is
-
-     function Uses_Extension(Ctx : Context; Extension : String) return Boolean is
-        -- Returns true if the context should contain files with the given extension.
+    function Uses_Extension (Ctx : Context; Extension : String) return Boolean is
+    --  Returns true if the context should contain files with the given extension.
     begin
-        return Ctx.Extensions.Contains(Ada.Strings.Unbounded.To_Unbounded_String(Extension));
+        return Ctx.Extensions.Contains (Ada.Strings.Unbounded.To_Unbounded_String (Extension));
     end Uses_Extension;
 
-    function Add_File(Ctx : in out Context; Next_Entry : Ada.Directories.Directory_Entry_Type) return Boolean is
+    function Add_File (Ctx : in out Context; Next_Entry : Ada.Directories.Directory_Entry_Type) return Boolean is
         use Ada.Directories;
     begin
-        if Ada.Directories.Kind (Next_Entry) = Ada.Directories.Ordinary_File and Uses_Extension(Ctx, Ada.Directories.Extension(Ada.Directories.Simple_Name(Next_Entry))) then
+        if Ada.Directories.Kind (Next_Entry) = Ada.Directories.Ordinary_File and
+            Uses_Extension (Ctx, Ada.Directories.Extension (Ada.Directories.Simple_Name (Next_Entry)))
+        then
             declare
                 Lines : String_Vectors.Vector := String_Vectors.Empty_Vector;
             begin
-                if Read_Lines(Ada.Directories.Full_Name(Next_Entry), Lines) then
+                if Read_Lines (Ada.Directories.Full_Name (Next_Entry), Lines) then
                     if Ctx.Files.Contains
-                        (Ada.Strings.Unbounded.To_Unbounded_String
-                             (Ada.Directories.Full_Name (Next_Entry)))
+                            (Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)))
                     then
                         Ctx.Files.Replace
-                            (Ada.Strings.Unbounded.To_Unbounded_String
-                                 (Ada.Directories.Full_Name (Next_Entry)),
-                             Lines);
+                            (Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)), Lines);
                     else
                         Ctx.Files.Insert
-                            (Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)),
-                             Lines);
+                            (Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)), Lines);
                     end if;
                     Ada.Text_IO.Put_Line ("Next File is: " & Ada.Directories.Full_Name (Next_Entry));
                 else
@@ -39,41 +36,38 @@ package body SP.Contexts is
         end if;
 
         if Ada.Directories.Kind (Next_Entry) = Ada.Directories.Directory
-            and then not Ctx.Refresh(Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)))
+            and then not Ctx.Refresh
+                (Ada.Strings.Unbounded.To_Unbounded_String (Ada.Directories.Full_Name (Next_Entry)))
         then
             return False;
         end if;
         return True;
     end Add_File;
 
-
-    function Is_Current_Or_Parent_Directory(Dir_Entry : Ada.Directories.Directory_Entry_Type) return Boolean is
-        -- Return true if the entry is "." or "..".
-        Name : constant String := Ada.Directories.Simple_Name(Dir_Entry);
+    function Is_Current_Or_Parent_Directory (Dir_Entry : Ada.Directories.Directory_Entry_Type) return Boolean is
+        --  Return true if the entry is "." or "..".
+        Name : constant String := Ada.Directories.Simple_Name (Dir_Entry);
     begin
-        return Ada.Directories.Hierarchical_File_Names.Is_Parent_Directory_Name(Name) or else
-            Ada.Directories.Hierarchical_File_Names.Is_Current_Directory_Name(Name);
+        return
+            Ada.Directories.Hierarchical_File_Names.Is_Parent_Directory_Name (Name)
+            or else Ada.Directories.Hierarchical_File_Names.Is_Current_Directory_Name (Name);
     end Is_Current_Or_Parent_Directory;
 
-    function Refresh
-        (Ctx : in out Context; Starting_Dir : Ada.Strings.Unbounded.Unbounded_String)
-          return Boolean is
+    function Refresh (Ctx : in out Context; Starting_Dir : Ada.Strings.Unbounded.Unbounded_String) return Boolean is
         --  Refreshes the list of files stored in the context.
         use Ada.Directories;
-        Search     : Search_Type;
+        Search : Search_Type;
         Next_Entry : Directory_Entry_Type;
-        Filter     : constant Filter_Type := (Ordinary_File | Directory => True, others => False);
+        Filter : constant Filter_Type := (Ordinary_File | Directory => True, others => False);
     begin
         Ada.Directories.Start_Search
-            (Search  => Search,
-             Directory => Ada.Strings.Unbounded.To_String (Starting_Dir),
-             Pattern => "*",
+            (Search => Search, Directory => Ada.Strings.Unbounded.To_String (Starting_Dir), Pattern => "*",
              Filter => Filter);
         while Ada.Directories.More_Entries (Search) loop
             Ada.Directories.Get_Next_Entry (Search, Next_Entry);
-            if Is_Current_Or_Parent_Directory(Next_Entry) then
+            if Is_Current_Or_Parent_Directory (Next_Entry) then
                 null;
-            elsif not Add_File(Ctx, Next_Entry) then
+            elsif not Add_File (Ctx, Next_Entry) then
                 return False;
             end if;
         end loop;
@@ -88,7 +82,9 @@ package body SP.Contexts is
     function List (Ctx : in Context) return Boolean is
     begin
         for Elem in Ctx.Files.Iterate loop
-            Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String(File_Maps.Key (Elem)) & " : " & Integer'Image(Integer(File_Maps.Element(Elem).Length)));
+            Ada.Text_IO.Put_Line
+                (Ada.Strings.Unbounded.To_String (File_Maps.Key (Elem)) & " : " &
+                 Integer'Image (Integer (File_Maps.Element (Elem).Length)));
         end loop;
         return True;
     end List;
@@ -96,24 +92,23 @@ package body SP.Contexts is
     function Add_Extensions (Ctx : in out Context; Extensions : in String_Vectors.Vector) return Boolean is
     begin
         for Ext of Extensions loop
-            if not Ctx.Extensions.Contains(Ext) then
-                Ctx.Extensions.Append(Ext);
+            if not Ctx.Extensions.Contains (Ext) then
+                Ctx.Extensions.Append (Ext);
             end if;
         end loop;
 
-        Ada.Text_IO.Put_Line("Extensions:");
+        Ada.Text_IO.Put_Line ("Extensions:");
         for Ext of Ctx.Extensions loop
-            Ada.Strings.Unbounded.Text_IO.Put_Line(Ext);
+            Ada.Strings.Unbounded.Text_IO.Put_Line (Ext);
         end loop;
         return True;
     end Add_Extensions;
 
-
     function Remove_Extensions (Ctx : in out Context; Extensions : in String_Vectors.Vector) return Boolean is
     begin
         for Ext of Extensions loop
-            if Ctx.Extensions.Contains(Ext) then
-                Ctx.Extensions.Delete(Ctx.Extensions.Find_Index(Ext));
+            if Ctx.Extensions.Contains (Ext) then
+                Ctx.Extensions.Delete (Ctx.Extensions.Find_Index (Ext));
             end if;
         end loop;
         return True;
@@ -138,17 +133,17 @@ package body SP.Contexts is
         declare
             Width : Integer;
         begin
-            Width := Integer'Value(Ada.Strings.Unbounded.To_String(Words.First_Element));
+            Width := Integer'Value (Ada.Strings.Unbounded.To_String (Words.First_Element));
             if Width > 0 then
-                Ada.Text_IO.Put_Line ("Context set to " & Integer'Image(Width));
-                Ctx.Width := Context_Width(Width);
+                Ada.Text_IO.Put_Line ("Context set to " & Integer'Image (Width));
+                Ctx.Width := Context_Width (Width);
             else
                 Ada.Text_IO.Put_Line ("Context must be one line or greater.");
             end if;
             return True;
         exception
             when others =>
-                Ada.Text_IO.Put_Line("Invalid context: " & Ada.Strings.Unbounded.To_String(Words.First_Element));
+                Ada.Text_IO.Put_Line ("Invalid context: " & Ada.Strings.Unbounded.To_String (Words.First_Element));
                 return False;
         end;
     end Set_Context_Width;
--- src/sp-contexts.ads
+++ src/sp-contexts.ads
@@ -10,37 +10,38 @@ package SP.Contexts is
     type Context_Width is new Natural;
     Full_File_Width : constant := 0;
 
-    package File_Maps is new Ada.Containers.Ordered_Maps(Key_Type     => Ada.Strings.Unbounded.Unbounded_String,
-                                                         Element_Type => String_Vectors.Vector,
-                                                         "<" => Ada.Strings.Unbounded."<",
-                                                         "=" => String_Vectors."="
-                                                        );
+    package File_Maps is new Ada.Containers.Ordered_Maps
+        (Key_Type => Ada.Strings.Unbounded.Unbounded_String, Element_Type => String_Vectors.Vector,
+         "<" => Ada.Strings.Unbounded."<", "=" => String_Vectors."=");
 
     type Context is tagged record
-        --  A description of the current search. This includes all of the files of the starting
-        --  point, their contents, as well as the currently applied filters.
+        --  A description of the current search. This includes all of the files of the starting point, their contents,
+        --  as well as the currently applied filters.
 
         Starting_Dir : Ada.Strings.Unbounded.Unbounded_String;
         --  Absolute path of the starting directory.
 
         Files : File_Maps.Map;
-        -- Cached contents of all files.
+        --  Cached contents of all files.
 
         Extensions : String_Vectors.Vector;
-        -- List of extensions, without the "." of all of the types of files which should be
-        -- considered in the search list.
+        --  List of extensions, without the "." of all of the types of files which should be considered in the search
+        --  list.
 
         Width : Context_Width;
-        -- The number of lines above and below a positive search result to include
-        -- or not include.
+        --  The number of lines above and below a positive search result to include or not include.
     end record;
 
     function Add_Extensions (Ctx : in out Context; Extensions : in String_Vectors.Vector) return Boolean;
     function Remove_Extensions (Ctx : in out Context; Extensions : in String_Vectors.Vector) return Boolean;
-    function Uses_Extension(Ctx : Context; Extension : String) return Boolean;
-    function Add_File(Ctx : in out Context; Next_Entry : Ada.Directories.Directory_Entry_Type) return Boolean;
-    function Refresh(Ctx : in out Context; Starting_Dir : Ada.Strings.Unbounded.Unbounded_String) return Boolean;
+    function Uses_Extension (Ctx : Context; Extension : String) return Boolean;
+    function Add_File (Ctx : in out Context; Next_Entry : Ada.Directories.Directory_Entry_Type) return Boolean;
+    function Refresh (Ctx : in out Context; Starting_Dir : Ada.Strings.Unbounded.Unbounded_String) return Boolean;
     function List (Ctx : in Context) return Boolean;
     function Set_Context_Width (Ctx : in out Context; Words : in String_Vectors.Vector) return Boolean;
+
+    --  Gets the current set of matching files.
+
+    --  Gets the current set of matching lines.
 
 end SP.Contexts;
