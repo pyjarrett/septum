315d7a26f62126018682f4448b0ca46e3f5112e7
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-05-14 21:38:04 -0400

    Add missing files.

---
+++ src/sp-cache.adb
@@ -0,0 +1,4 @@
+package body SP.Cache is
+    protected body File_Cache is
+    end File_Cache;
+end SP.Cache;
---
+++ src/sp-cache.ads
@@ -0,0 +1,6 @@
+package SP.Cache is
+    -- File caching operations.  Loads files in the background as needed.
+    protected type File_Cache is
+
+    end File_Cache;
+end SP.Cache;
