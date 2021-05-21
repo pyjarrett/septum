6f3f9f9f88d0a91561524ce5123da75daad3cd90
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-05-20 22:45:44 -0400

    Add more recognized text file extensions.

--- src/sp-cache.adb
+++ src/sp-cache.adb
@@ -13,10 +13,19 @@ package body SP.Cache is
     function Is_Text (File_Name : String) return Boolean is
         -- This is probably better written to look at encoding (such as invalid sequences in UTF-8, etc.)
         -- instead of being a hodgepodge of various formats I know that I care about right now.
+        -- TODO: Adding more file types I care about now, this needs to be fixed properly.
         Ext : constant Ada.Strings.Unbounded.Unbounded_String :=
             To_Unbounded_String (Ada.Directories.Extension (File_Name));
         Known_Text : constant array (Positive range <>) of Ada.Strings.Unbounded.Unbounded_String :=
-            (+"ads", +"adb", +"c", +"cpp", +"h", +"hpp", +"hh", +"cs");
+            (+"ads", +"adb",          -- Ada
+             +"c", +"h",              -- c
+             +"cpp", "C",             -- C++
+             +"hpp", +"hh", +"inl",
+             +"cs",                   -- C#
+             +"hs",                   -- Haskell
+             +"py",                   -- Python
+             +"rs",                   -- Rust
+            );
     begin
         return (for some X of Known_Text => Ext = X);
     end Is_Text;
