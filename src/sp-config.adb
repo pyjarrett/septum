a539c45ff29a9629d2549dd22aa5836b897a5f47
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-06-13 17:36:40 -0400

    Swap to using Alire.

--- src/sp-config.adb
+++ src/sp-config.adb
@@ -20,8 +20,23 @@
 with Ada.Directories;
 with Ada.Strings.Unbounded;
 with GNATCOLL.VFS; use GNATCOLL.VFS;
+with Dir_Iterators.Ancestor;
 
 package body SP.Config is
+    package AD renames Ada.Directories;
+    package ASU renames Ada.Strings.Unbounded;
+
+    use type Ada.Directories.File_Kind;
+    use type ASU.Unbounded_String;
+
+    Config_Dir_Name  : constant String := ".septum";
+    Config_File_Name : constant String := ".config";
+
+    function Is_File (Target : String) return Boolean is
+    begin
+        return AD.Exists (Target) and then AD.Kind (Target) = AD.Ordinary_File;
+    end Is_File;
+
     -- Septum data is stored locally in the Next_Dir working directory on load or in the home directory of the user
     -- running the command.  This allows users to maintain general configuration in their home directory based
     -- on the settings they want to work with, and then have per-project settings that they can use.
@@ -30,50 +45,38 @@ package body SP.Config is
     -- Containing_Directory/
     --     .septum/                 Directory to contain all Septum related data.
     --         .config              Commands to run on startup.
-
-    Config_Dir       : constant Filesystem_String := ".septum";
-    Config_File_Name : constant Filesystem_String := ".config";
-
-    function Closest_Config (Dir_Name : String) return Virtual_File with
-        Pre => Ada.Directories.Exists (Dir_Name),
-        Post => Is_Regular_File (Closest_Config'Result) or else Closest_Config'Result = No_File
+    function Closest_Config (Dir_Name : String) return ASU.Unbounded_String with
+        Pre  => AD.Exists (Dir_Name),
+        Post => (Closest_Config'Result = ASU.Null_Unbounded_String)
+        or else Is_File (ASU.To_String (Closest_Config'Result))
     is
-        -- Looks for a config in the tree starting at Dir_Name, which might be somewhere in the project tree.
-        Next_Dir : Virtual_File := Create (+Dir_Name);
+        Ancestors  : constant Dir_Iterators.Ancestor.Ancestor_Dir_Walk := Dir_Iterators.Ancestor.Walk (Dir_Name);
+        Next_Trial : ASU.Unbounded_String;
     begin
-        return Result : Virtual_File do
-            loop
-                Result := Next_Dir / Config_Dir / Config_File_Name;
-                if Result.Is_Regular_File then
-                    return;
-                end if;
-
-                -- Move up the tree.
-                Next_Dir := Next_Dir.Get_Parent;
-                if Next_Dir = No_File then
-                    -- Reached the top, so no parent exists.
-                    Result := No_File;
-                    return;
-                end if;
-            end loop;
-        end return;
+        for Ancestor of Ancestors loop
+            Next_Trial := ASU.To_Unbounded_String (Ancestor & "/" & Config_Dir_Name & "/" & Config_File_Name);
+            if Is_File (ASU.To_String (Next_Trial)) then
+                return Next_Trial;
+            end if;
+        end loop;
+        return ASU.Null_Unbounded_String;
     end Closest_Config;
 
     function Config_Locations return String_Vectors.Vector is
-        use Ada.Strings.Unbounded;
-        Home_Dir_Config    : constant Virtual_File := Get_Home_Directory / Config_Dir / Config_File_Name;
-        Current_Dir_Config : constant Virtual_File := Closest_Config (+Get_Current_Dir.Full_Name);
+        Home_Dir_Config : constant ASU.Unbounded_String :=
+            ASU.To_Unbounded_String
+                (+Get_Home_Directory.Full_Name.all & "/" & Config_Dir_Name & "/" & Config_File_Name);
+        Current_Dir_Config : constant ASU.Unbounded_String := Closest_Config (Ada.Directories.Current_Directory);
     begin
         return V : String_Vectors.Vector do
             -- Look for the global user config.
-            if Is_Regular_File (Home_Dir_Config) then
-                V.Append (To_Unbounded_String (+Home_Dir_Config.Full_Name));
+            if Is_File (ASU.To_String (Home_Dir_Config)) then
+                V.Append (Home_Dir_Config);
             end if;
 
-            if Is_Regular_File (Current_Dir_Config) then
-                V.Append (To_Unbounded_String (+Current_Dir_Config.Full_Name));
+            if Is_File (ASU.To_String (Current_Dir_Config)) then
+                V.Append (Current_Dir_Config);
             end if;
         end return;
     end Config_Locations;
-
 end SP.Config;
