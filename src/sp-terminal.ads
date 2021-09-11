c163e8932bfef0dc9d2135d030da9366098163dd
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-09-11 15:27:30 -0400

    Replace GNATCOLL.Terminal with Trendy_Terminal.

--- src/sp-terminal.ads
+++ src/sp-terminal.ads
@@ -17,8 +17,7 @@
 with Ada.Strings.Unbounded;
 with Ada.Text_IO;
 with Ada.Text_IO.Unbounded_IO;
-
-with GNATCOLL.Terminal;
+with Trendy_Terminal.VT100;
 
 package SP.Terminal is
     -- Functions for operations to the terminal. This hides the usage of Ada.Text_IO and may silently ignore
@@ -26,29 +25,24 @@ package SP.Terminal is
     --
     -- This module also hides dependencies on unbounded IO.
 
-    procedure Put (Str : String) renames Ada.Text_IO.Put;
+    procedure Put (Str : String) renames Trendy_Terminal.Put;
     procedure Put (Str : Ada.Strings.Unbounded.Unbounded_String) renames Ada.Text_IO.Unbounded_IO.Put;
 
-    procedure Put_Line (Str : String) renames Ada.Text_IO.Put_Line;
-    procedure Put_Line (Str : Ada.Strings.Unbounded.Unbounded_String) renames Ada.Text_IO.Unbounded_IO.Put_Line;
+    procedure Put_Line (Str : String) renames Trendy_Terminal.Put_Line;
+    procedure Put_Line (Str : Ada.Strings.Unbounded.Unbounded_String) renames Trendy_Terminal.Put_Line;
 
     function Get_Line return String renames Ada.Text_IO.Get_Line;
-    function Get_Line return Ada.Strings.Unbounded.Unbounded_String renames Ada.Text_IO.Unbounded_IO.Get_Line;
 
-    procedure New_Line (Spacing : in Ada.Text_IO.Positive_Count := 1) renames Ada.Text_IO.New_Line;
+    procedure New_Line (Spacing : Positive := 1) renames Trendy_Terminal.New_Line;
 
-    procedure Set_Col (Spacing : in Ada.Text_IO.Positive_Count) renames Ada.Text_IO.Set_Col;
+    procedure Set_Col (Spacing : Positive) renames Trendy_Terminal.Set_Col;
 
-    procedure Clear_Line;
+    procedure Clear_Line renames Trendy_Terminal.Clear_Line;
 
     -- I'm not convinced that these aren't useful. I haven't figured out how best to deal with the really long and
     -- verbose terminology of Ada.Strings.Unbounded.Unbounded_String.
 
     --  function "&" (A : String; B : Unbounded_String) return Unbounded_String renames Ada.Strings.Unbounded."&";
     --  function "&" (Ada : Unbounded_String; B : String) return Unbounded_String renames Ada.Strings.Unbounded."&";
-
-private
-
-    Stdout_Term_Info : GNATCOLL.Terminal.Terminal_Info;
 
 end SP.Terminal;
