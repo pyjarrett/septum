b9cf0b7c44760eaee516d6ca28838ecf87d28be2
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-09-12 10:46:42 -0400

    Simplify SP.Strings.

--- src/sp-strings.ads
+++ src/sp-strings.ads
@@ -16,40 +16,40 @@
 
 with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Vectors;
-
 with Ada.Strings.Unbounded;
 
 package SP.Strings is
+    package ASU renames Ada.Strings.Unbounded;
+
     package String_Sets is new Ada.Containers.Ordered_Sets
-        (Element_Type => Ada.Strings.Unbounded.Unbounded_String, "<" => Ada.Strings.Unbounded."<",
-         "="          => Ada.Strings.Unbounded."=");
+        (Element_Type => ASU.Unbounded_String, "<" => ASU."<",
+         "="          => ASU."=");
     package String_Vectors is new Ada.Containers.Vectors
-        (Index_Type => Positive, Element_Type => Ada.Strings.Unbounded.Unbounded_String,
-         "="        => Ada.Strings.Unbounded."=");
+        (Index_Type => Positive, Element_Type => ASU.Unbounded_String,
+         "="        => ASU."=");
 
-    function Zip (Left : SP.Strings.String_Vectors.Vector; Right : SP.Strings.String_Vectors.Vector) return Ada.Strings.Unbounded.Unbounded_String;
-    function Format_Array (S : SP.Strings.String_Vectors.Vector) return Ada.Strings.Unbounded.Unbounded_String;
+    function Zip (Left, Right : String_Vectors.Vector) return ASU.Unbounded_String;
+    function Format_Array (S : String_Vectors.Vector) return ASU.Unbounded_String;
 
-    function Common_Prefix_Length
-        (A : Ada.Strings.Unbounded.Unbounded_String; B : Ada.Strings.Unbounded.Unbounded_String) return Natural with
-        Post => Common_Prefix_Length'Result <
-        Natural'Max (Ada.Strings.Unbounded.Length (A), Ada.Strings.Unbounded.Length (B));
+    function Common_Prefix_Length (A, B : ASU.Unbounded_String) return Natural
+    with
+        Post => Common_Prefix_Length'Result < Natural'Max (ASU.Length (A), ASU.Length (B));
 
-    function Is_Quoted (S : String) return Boolean;
     -- Quoted strings must start and end with either a single or a double quote.
+    function Is_Quoted (S : String) return Boolean;
 
     type Exploded_Line is record
         -- The first space is "Leading spacing"
         -- Spaces(i) is what preceeds Words(i)
-        Spacers : SP.Strings.String_Vectors.Vector;
-        Words   : SP.Strings.String_Vectors.Vector;
+        Spacers : String_Vectors.Vector;
+        Words   : String_Vectors.Vector;
     end record;
 
     -- TODO: This will eventually need to be rewritten to account for multi-byte
     -- sequences in UTF-8.  Incurring technical debt here on purpose to try to get
     -- the command line formatter stood up more quickly.
     function Make (S : String) return Exploded_Line;
-    function Get_Word (E : Exploded_Line; Index : Positive) return String is (Ada.Strings.Unbounded.To_String (E.Words (Index)));
+    function Get_Word (E : Exploded_Line; Index : Positive) return String is (ASU.To_String (E.Words (Index)));
     function Num_Words (E : Exploded_Line) return Natural is (Natural (E.Words.Length));
 
 end SP.Strings;
