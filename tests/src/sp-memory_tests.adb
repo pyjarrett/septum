4a952687cc7ad29d236ef17284c60c48dc64796c
Paul Jarrett <jarrett.paul.young@gmail.com>
2021-12-30 23:52:14 -0500

    Update tests to use latest trendy_test operations.

--- tests/src/sp-memory_tests.adb
+++ tests/src/sp-memory_tests.adb
@@ -1,15 +1,15 @@
 with SP.Memory;
-with Trendy_Test.Assertions;
+with Trendy_Test.Assertions.Integer_Assertions;
+
+use Trendy_Test.Assertions;
+use Trendy_Test.Assertions.Integer_Assertions;
 
 package body SP.Memory_Tests is
-
-    package TT renames Trendy_Test;
-    use Trendy_Test.Assertions.Integer_Assertions;
 
     type Int_Access is access Integer;
     package Int_Ptr is new SP.Memory (T => Integer, T_Access => Int_Access);
 
-    procedure Test_Count (Op : in out TT.Operation'Class) is
+    procedure Test_Count (Op : in out Trendy_Test.Operation'Class) is
     begin
         Op.Register;
 
@@ -19,32 +19,32 @@ package body SP.Memory_Tests is
         begin
             Assert_EQ (Op, Integer (I.Count), 0);
             Assert_EQ (Op, Integer (J.Count), 0);
-            Op.Assert (not I.Is_Valid);
-            Op.Assert (not J.Is_Valid);
+            Assert (Op, not I.Is_Valid);
+            Assert (Op, not J.Is_Valid);
 
             I := Int_Ptr.Make (new Integer'(5));
             Assert_EQ (Op, Integer (I.Count), 1);
             Assert_EQ (Op, Integer (J.Count), 0);
-            Op.Assert (I.Is_Valid);
-            Op.Assert (not J.Is_Valid);
+            Assert (Op, I.Is_Valid);
+            Assert (Op, not J.Is_Valid);
 
             J := I;
             Assert_EQ (Op, Integer (I.Count), 2);
             Assert_EQ (Op, Integer (J.Count), 2);
-            Op.Assert (I.Is_Valid);
-            Op.Assert (J.Is_Valid);
+            Assert (Op, I.Is_Valid);
+            Assert (Op, J.Is_Valid);
 
             I.Reset;
             Assert_EQ (Op, Integer (I.Count), 0);
             Assert_EQ (Op, Integer (J.Count), 1);
-            Op.Assert (not I.Is_Valid);
-            Op.Assert (J.Is_Valid);
+            Assert (Op, not I.Is_Valid);
+            Assert (Op, J.Is_Valid);
 
             J.Reset;
             Assert_EQ (Op, Integer (I.Count), 0);
             Assert_EQ (Op, Integer (J.Count), 0);
-            Op.Assert (not I.Is_Valid);
-            Op.Assert (not J.Is_Valid);
+            Assert (Op, not I.Is_Valid);
+            Assert (Op, not J.Is_Valid);
         end;
     end Test_Count;
 
--- tests/src/sp-strings-tests.adb
+++ tests/src/sp-strings-tests.adb
@@ -1,17 +1,17 @@
 with Ada.Characters.Latin_1;
 with Ada.Strings.Unbounded;
-with Trendy_Test.Assertions;
+with Trendy_Test.Assertions.Integer_Assertions;
 
 package body SP.Strings.Tests is
 
     package ASU renames Ada.Strings.Unbounded;
-    package TT renames Trendy_Test;
+
     use Trendy_Test.Assertions;
     use Trendy_Test.Assertions.Integer_Assertions;
 
     function "+" (S : String) return ASU.Unbounded_String renames ASU.To_Unbounded_String;
 
-    procedure Test_String_Split (Op : in out TT.Operation'Class) is
+    procedure Test_String_Split (Op : in out Trendy_Test.Operation'Class) is
         E : Exploded_Line;
     begin
         Op.Register;
@@ -27,40 +27,41 @@ package body SP.Strings.Tests is
         Assert_EQ (Op, Get_Word (E, 6), "with--content");
     end Test_String_Split;
 
-    procedure Test_Is_Quoted (Op : in out TT.Operation'Class) is
+    procedure Test_Is_Quoted (Op : in out Trendy_Test.Operation'Class) is
         use Ada.Characters.Latin_1;
     begin
         Op.Register;
 
-        Op.Assert (not Is_Quoted(""));
-        Op.Assert (not Is_Quoted ("not quoted"));
+        Assert (Op, not Is_Quoted(""));
+        Assert (Op, not Is_Quoted ("not quoted"));
 
         -- Unbalanced "
-        Op.Assert (not Is_Quoted (Quotation & "some text"));
-        Op.Assert (not Is_Quoted ("some text" & Quotation));
+        Assert (Op, not Is_Quoted (Quotation & "some text"));
+        Assert (Op, not Is_Quoted ("some text" & Quotation));
 
         -- Unbalanced '
-        Op.Assert (not Is_Quoted (Apostrophe & "some text"));
-        Op.Assert (not Is_Quoted ("some text" & Apostrophe));
+        Assert (Op, not Is_Quoted (Apostrophe & "some text"));
+        Assert (Op, not Is_Quoted ("some text" & Apostrophe));
 
         -- Mismatched ' and "
-        Op.Assert (not Is_Quoted (Quotation & "some text" & Apostrophe));
-        Op.Assert (not Is_Quoted (Apostrophe & "some text" & Quotation));
+        Assert (Op, not Is_Quoted (Quotation & "some text" & Apostrophe));
+        Assert (Op, not Is_Quoted (Apostrophe & "some text" & Quotation));
 
         -- Matched " and '
-        Op.Assert (Is_Quoted (Apostrophe & "some text" & Apostrophe));
-        Op.Assert (Is_Quoted (Quotation & "some text" & Quotation));
+        Assert (Op, Is_Quoted (Apostrophe & "some text" & Apostrophe));
+        Assert (Op, Is_Quoted (Quotation & "some text" & Quotation));
 
         -- Internal " or '
-        Op.Assert (Is_Quoted (Apostrophe & Quotation & "some text" & Quotation & Apostrophe));
-        Op.Assert (Is_Quoted (Apostrophe & Quotation & "some text" & Quotation & Apostrophe));
+        Assert (Op, Is_Quoted (Apostrophe & Quotation & "some text" & Quotation & Apostrophe));
+        Assert (Op, Is_Quoted (Apostrophe & Quotation & "some text" & Quotation & Apostrophe));
     end Test_Is_Quoted;
 
-    procedure Test_Common_Prefix_Length (Op : in out TT.Operation'Class) is
+    procedure Test_Common_Prefix_Length (Op : in out Trendy_Test.Operation'Class) is
+        package Trendy_TestI renames Trendy_Test.Assertions.Integer_Assertions;
     begin
         Op.Register;
 
-        Assert_EQ (Op, Common_Prefix_Length(+"", +"SP.Strings"), 0);
+        Trendy_TestI.Assert_EQ (Op, Common_Prefix_Length(+"", +"SP.Strings"), 0);
         Assert_EQ (Op, Common_Prefix_Length(+"SP.Strings", +""), 0);
         Assert_EQ (Op, Common_Prefix_Length(+"", +""), 0);
 
