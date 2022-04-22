aa9a14f413b772d979e07f1096504bf139be8a1f
Paul Jarrett <jarrett.paul.young@gmail.com>
2022-04-22 17:42:46 -0400

    Adds mac files.

---
+++ src/mac/sp-platform.adb
@@ -0,0 +1,21 @@
+with Ada.Directories;
+with Ada.Environment_Variables;
+
+package body SP.Platform is
+
+    function Home_Dir return String is
+        package Env renames Ada.Environment_Variables;
+        User_Profile : constant String := "HOME";
+    begin
+        if Env.Exists (User_Profile) then
+            return Ada.Directories.Full_Name (Env.Value (User_Profile));
+        else
+            -- TODO: Add a better fallback case here.
+            return "";
+        end if;
+    end Home_Dir;
+
+    function Path_Separator return Character is ('/');
+    function Path_Opposite_Separator return Character is ('\');
+
+end SP.Platform;
